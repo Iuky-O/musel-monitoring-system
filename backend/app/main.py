4a2e02168b755308fb574b136f9370b1f2bad97d
Letícia <117243523+leh826@users.noreply.github.com>
2025-03-21 17:14:39 -0300

    uptade: main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.api.test_connection import router as test_router
+
 from app.api.vision import router as vision_router
 
 app = FastAPI()
 
+app.include_router(test_router, prefix="/api")
 app.include_router(vision_router, prefix="/vision", tags=["vision"])
+
+@app.get("/")
+def home():
+    return {"message": "API rodando!"}
