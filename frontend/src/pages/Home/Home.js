356c15161d475fc0fcd0b598fcbcde19208b5a3c
Leh826 <dev.rocha826@gmail.com>
2025-03-26 23:12:25 -0300

    Add: frontend base structure added

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function Home() {
+    const [artwork, setArtwork] = useState(null);
+    const socket = new WebSocket("ws://localhost:8000/ws");
+
+    useEffect(() => {
+        socket.onmessage = (event) => {
+            setArtwork(JSON.parse(event.data));
+        };
+    }, []);
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            {artwork ? (
+                <>
+                    <h1>{artwork.title}</h1>
+                    <p>{artwork.description}</p>
+                    <img src={artwork.image_url} alt={artwork.title} style={{ width: "300px" }} />
+                    <audio controls src={artwork.audio_url}></audio>
+                </>
+            ) : (
+                <p>Aproxime-se de uma obra para saber mais!</p>
+            )}
+        </div>
+    );
+}
+
+export default Home;
