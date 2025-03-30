fe0df9b6fe0122d4bde32a680173a2d6a090df45
Leh826 <dev.rocha826@gmail.com>
2025-03-30 17:52:48 -0300

    add: foi atualizado o main com CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 from .api.exibicao.interacoes import router as interacoes_router
 from app.api.admin import obras, visitas
 from app.api.ws import router as ws_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens (atenção com segurança)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os headers
+)
+
 
 app.include_router(interacoes_router,prefix="/interacao", tags=["Interacao"])
 app.include_router(obras.router, prefix="/admin", tags=["obras"])
